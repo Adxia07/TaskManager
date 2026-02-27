fb630b06d248b2eee99b835c5497488499e56309
Adxia07 <Alex.s.0705@mail.ru>
2026-02-28 01:39:00 +0300

    Добавил класс CoreIndexConverter

---
+++ TaskManager/Converters/CoreIndexConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TaskManager.Converters
+{
+    public class CoreIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int index)
+            {
+                return $"Ядро {index}";
+            }
+            return "Ядро";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
