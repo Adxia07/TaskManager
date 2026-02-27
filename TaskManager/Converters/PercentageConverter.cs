0a7bf63e549c18347545b048c9ad499c2d887e9b
Adxia07 <Alex.s.0705@mail.ru>
2026-02-28 01:40:07 +0300

    Добавил класс PercentageConverter

---
+++ TaskManager/Converters/PercentageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TaskManager.Converters
+{
+    public class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return $"{doubleValue:F1}%";
+            }
+            return "0%";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
