f60d62c9cab81fb5e4c8ca1f7a938f5afc016aa0
Adxia07 <Alex.s.0705@mail.ru>
2026-02-28 01:22:08 +0300

    Создал класс ProcessInfo

---
+++ TaskManager/Models/ProcessInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TaskManager.Models
+{
+    public class ProcessInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ProcessPriorityClass Priority { get; set; }
+        public long MemoryUsage { get; set; }
+        public int ThreadCount { get; set; }
+        public TimeSpan CpuTime { get; set; }
+        public int? ParentId { get; set; }
+        public IntPtr AffinityMask { get; set; }
+
+    }
+}
---
+++ TaskManager/Models/ProcessTreeNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using TaskManager.Models;
+
+namespace TaskManager.Models
+{
+    public class ProcessTreeNode
+    {
+        public ProcessInfo Process { get; set; }
+        public ObservableCollection<ProcessTreeNode> Children { get; set; }
+            = new ObservableCollection<ProcessTreeNode>();
+    }
+}
