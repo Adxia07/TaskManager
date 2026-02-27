8d74c31cd45f04eee00c3b98187f0d2b2c2a013e
Adxia07 <Alex.s.0705@mail.ru>
2026-02-28 01:36:56 +0300

    Создал класс AffinityHelper

---
+++ TaskManager/Utilities/AffinityHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskManager.Utilities
+{
+    public static class AffinityHelper
+    {
+        public static bool IsCoreEnabled(IntPtr mask, int coreIndex)
+        {
+            long maskValue = mask.ToInt64();
+            return (maskValue & (1L << coreIndex)) != 0;
+        }
+
+        public static IntPtr SetCoreMask(bool[] cores)
+        {
+            long mask = 0;
+            for (int i = 0; i < cores.Length; i++)
+            {
+                if (cores[i])
+                    mask |= (1L << i);
+            }
+            return new IntPtr(mask);
+        }
+    }
+}
