5749a4f61891090c1972fb0cbf5ba862ac3ac90d
Adxia07 <Alex.s.0705@mail.ru>
2026-02-28 01:33:51 +0300

    Создал класс BaseViewModel

---
+++ TaskManager/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TaskManager.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
